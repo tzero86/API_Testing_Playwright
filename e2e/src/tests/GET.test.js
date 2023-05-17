3d9647fcbbcbe05cead5414543d2f5c01e47964e
tzero86 <eliasmedina86@gmail.com>
2023-05-17 17:16:20 -0300

    First couple of GET tests

--- e2e/src/tests/GET.test.js
+++ e2e/src/tests/GET.test.js
@@ -2,5 +2,17 @@ import { expect, test } from '@playwright/test';
 
 
 test('Retrieve user posts.', async ({ request }) => {
+    const response = await request.get('/posts');
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+});
 
+test('Retrieve user post.', async ({ request }) => {
+    const response = await request.get('/posts/1');
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    expect(await response.json()).toEqual(expect.objectContaining({
+        "id": 1,
+        "userId": 1,
+    }));
 });
